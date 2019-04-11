f7088c8212ce82326e1464c0598425c143e7c4ae
Albert Stack <albertstack@hotmail.com>
2019-04-11 22:30:40 +0800

    Delete unnecessary comments

--- ComOfCppMATLAB/main.cpp
+++ ComOfCppMATLAB/main.cpp
@@ -22,11 +22,5 @@ int main(int argc, char* argv[])
 	}	
 	
 
-
-	//此处必须要先编译一个InitMatlabEngine()函数的单独进程。
-	//然后再调用MatlabEnginInterface的时候再来连接MATLAB引擎。这样才能将MATLAB引擎移到后台而没有弹出窗口。
-	//问题在于如何知道在另一个进程中InitMAtlabEngin()调用成功，并且另一个进程不能退出。
-	//如果C++在引用MATLAB引擎进行计算的时候如果出错，错误信息该怎么显示？
-
 	return 0;
 }
